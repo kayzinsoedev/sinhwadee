db30929d6eb52726f69c1594ae51804e3f9e36a2
FCSLAP-A065\Kay Zin Soe <kay@firstcom.com.sg>
2021-10-08 12:05:44 +0800

    crop image size

--- catalog/controller/extension/module/about_content.php
+++ catalog/controller/extension/module/about_content.php
@@ -10,11 +10,28 @@ class ControllerExtensionModuleAboutContent extends Controller {
         $slogans = $Modulehelper->get_field ( $oc, $modulename, $language_id, 'slogans');
         $main_categories = $Modulehelper->get_field ( $oc, $modulename, $language_id, 'main_categories');
 
+
         $data['slogans'] = array();
         if(!empty($slogans)) {
+						$this->load->model('tool/image');
             //usort($icons, array($this,"sortSlogans"));
-            $data['slogans'] = $slogans;
+						foreach ($slogans as $key => $value) {
+								$data['slogans'][] = array(
+										'id' => $value['id'],
+										'main_categories' => $value['main_categories'],
+										'top_image' => $this->model_tool_image->resize($value['top_image'], 613, 414),
+										'alignment' => $value['alignment'],
+										'main_title' => $value['main_title'],
+										'sub_title' => $value['sub_title'],
+										'desc_title' => $value['desc_title'],
+										'module_status' => $value['module_status'],
+								);
+						}
+
+            // $data['slogans'] = $slogans;
         }
+
+				// debug($data['slogans']);
 
 				$data['main_categories'] = array();
         if(!empty($main_categories)) {
--- catalog/controller/extension/module/services.php
+++ catalog/controller/extension/module/services.php
@@ -10,17 +10,38 @@ class ControllerExtensionModuleServices extends Controller {
         $services = $Modulehelper->get_field ( $oc, $modulename, $language_id, 'services');
 				$main_categories = $Modulehelper->get_field ( $oc, $modulename, $language_id, 'main_categories');
 
+
+
+
         $data['services'] = array();
         if(!empty($services)) {
-            //usort($services, array($this,"sortSlogans"));
-            $data['services'] = $services;
+
+						foreach ($services as $key => $value) {
+							// debug($value['top_image']);
+							$this->load->model('tool/image');
+
+									$data['services'][] = array(
+											'id' => $value['id'],
+											'main_categories' => $value['main_categories'],
+											'top_image' => $this->model_tool_image->resize($value['top_image'], 613, 414),
+											'alignment' => $value['alignment'],
+											'title' => $value['title'],
+											'description' => $value['description'],
+											'module_status' => $value['module_status'],
+									);
+						}
+            // $data['services'] = $services;
         }
+
+				// debug($data['services']);
+
 
 				$data['main_categories'] = array();
         if(!empty($main_categories)) {
             $data['main_categories'] = $main_categories;
         }
-	
+
+
 		return $this->load->view('extension/module/services', $data);
     }
 
